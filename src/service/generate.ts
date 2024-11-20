fa9441e9968ff3f99b1b85bc68664ae298f30d49
Ivan Filho <i@ivanfilho.com>
2024-11-20 10:50:09 -0300

    chore: fix lint errors

--- src/service/generate.ts
+++ src/service/generate.ts
@@ -1,11 +1,10 @@
 import { compile } from './compile'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
-import ts from 'typescript'
+import type ts from 'typescript'
 
 const RETRY_DELAY = 1000
 const MAX_RETRY_COUNT = 10
-
 
 export function generate(
 	info: ts.server.PluginCreateInfo,
@@ -14,25 +13,35 @@ export function generate(
 	retryCount = 0,
 ) {
 	const logger = info.project.projectService.logger
+
+	const retry = () => {
+		if (retryCount < MAX_RETRY_COUNT) {
+			logger.info(`safe-router: not ready yet, retrying in ${RETRY_DELAY}ms (${retryCount + 1}/${MAX_RETRY_COUNT})`)
+			setTimeout(
+				() => generate(info, appDir, routesFilePath, retryCount + 1),
+				RETRY_DELAY,
+			)
+		}
+	}
+
 	logger.info('safe-router: getting TypeScript program')
 	const program = info.languageService.getProgram()
 
-	const isReady = program?.getSourceFiles().some((sourceFile) => {
+	if (!program) {
+		return retry()
+	}
+
+	const isReady = program.getSourceFiles().some((sourceFile) => {
 		const normalizedAppDir = path.normalize(appDir)
 		return sourceFile.fileName.startsWith(normalizedAppDir)
 	})
 
-	if (!isReady && retryCount < MAX_RETRY_COUNT) {
-		logger.info(`safe-router: not ready yet, retrying in ${RETRY_DELAY}ms (${retryCount + 1}/${MAX_RETRY_COUNT})`)
-		setTimeout(
-			() => generate(info, appDir, routesFilePath, retryCount + 1),
-			RETRY_DELAY,
-		)
-		return
+	if (!isReady) {
+		return retry()
 	}
 
 	logger.info('safe-router: compiling routes file')
-	const routesFile = compile(program!, appDir)
+	const routesFile = compile(program, appDir)
 
 	fs.mkdirSync(path.dirname(routesFilePath), { recursive: true })
 	fs.writeFileSync(routesFilePath, routesFile, 'utf8')
