1270399010ddd50b279f720b6777e0f50bb8b6dd
Ivan Filho <i@ivanfilho.com>
2024-11-20 10:46:28 -0300

    fix: remove manual program creation

--- src/service/generate.ts
+++ src/service/generate.ts
@@ -14,28 +14,16 @@ export function generate(
 	retryCount = 0,
 ) {
 	const logger = info.project.projectService.logger
-	let program = info.languageService.getProgram()
 	logger.info('safe-router: getting TypeScript program')
+	const program = info.languageService.getProgram()
 
-	try {
-		if (!program) {
-			program = ts.createProgram({
-				rootNames: info.project.getRootFiles(),
-				options: info.project.getCompilerOptions(),
-			})
-		}
-	} catch (e) {
-		logger.info('safe-router: failed to get TypeScript program')
-		return
-	}
-
-	const isReady = program.getSourceFiles().some((sourceFile) => {
+	const isReady = program?.getSourceFiles().some((sourceFile) => {
 		const normalizedAppDir = path.normalize(appDir)
 		return sourceFile.fileName.startsWith(normalizedAppDir)
 	})
 
 	if (!isReady && retryCount < MAX_RETRY_COUNT) {
-		logger.info( `safe-router: not ready yet, retrying in ${RETRY_DELAY}ms (${retryCount + 1}/${MAX_RETRY_COUNT})`)
+		logger.info(`safe-router: not ready yet, retrying in ${RETRY_DELAY}ms (${retryCount + 1}/${MAX_RETRY_COUNT})`)
 		setTimeout(
 			() => generate(info, appDir, routesFilePath, retryCount + 1),
 			RETRY_DELAY,
@@ -44,7 +32,7 @@ export function generate(
 	}
 
 	logger.info('safe-router: compiling routes file')
-	const routesFile = compile(program, appDir)
+	const routesFile = compile(program!, appDir)
 
 	fs.mkdirSync(path.dirname(routesFilePath), { recursive: true })
 	fs.writeFileSync(routesFilePath, routesFile, 'utf8')
